b221cb3f40a5c679b484de4e2de1cc5c08ae4f7f
Marce Persico <marcelo@paperpile.com>
2023-11-25 00:27:36 -0500

    fix: cms login

--- src/components/customHead.tsx
+++ src/components/customHead.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
+const script = `if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}`;
+
 const CustomHead: React.FC<{ title: string; description: string }> = ({ title, description }) => {
   return (
     <>
       <html lang="es" />
+      <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       <title>{title}</title>
       <meta name="description" content={description} />
+      <script>
+        {script}
+      </script>
     </>
   );
 };
