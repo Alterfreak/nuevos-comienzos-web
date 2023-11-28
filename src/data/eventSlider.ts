e6a771da83b7d41c87b8ab107795ac297e29523d
Marce Persico <marcelo@paperpile.com>
2023-11-28 09:27:43 -0500

    fix: events slider images

--- src/data/eventSlider.ts
+++ src/data/eventSlider.ts
@@ -1,27 +1,27 @@
 const events = [
   {
     id: 'miercoles',
-    image: '../images/ad-culto.webp',
+    image: '/images/ad-culto.webp',
     description: 'placeholder',
   },
   {
     id: 'lunes',
-    image: '../images/ad-alborada.webp',
+    image: '/images/ad-alborada.webp',
     description: 'placeholder',
   },
   {
     id: 'martes',
-    image: '../images/ad-estudio-biblico.webp',
+    image: '/images/ad-estudio-biblico.webp',
     description: 'placeholder',
   },
   {
     id: 'jueves',
-    image: '../images/ad-miercoles-oracion.webp',
+    image: '/images/ad-miercoles-oracion.webp',
     description: 'placeholder',
   },
   {
     id: 'viernes',
-    image: '../images/ad-jni.webp',
+    image: '/images/ad-jni.webp',
     description: 'placeholder',
   },
 ];
