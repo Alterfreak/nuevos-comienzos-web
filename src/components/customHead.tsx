a818206dff344f490178a306010004bd1f89dcea
Marce Persico <marcelo@paperpile.com>
2023-11-29 15:55:26 -0500

    feature: og meta tags

--- src/components/customHead.tsx
+++ src/components/customHead.tsx
@@ -18,6 +18,12 @@ const CustomHead: React.FC<{ title: string; description: string }> = ({ title, d
       <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
       <title>{title}</title>
       <meta name="description" content={description} />
+      <meta property="og:image" content="/images/ancho-blanco-norte-padding.webp" />
+      <meta property="og:title" content="Iglesia del Nazareno - Nuevos Comienzos" />
+      <meta property="og:type" content="website" />
+
+      <meta property="og:url" content="https://wwww.nuevos-comienzos.org" />
+      <meta property="og:description" content="Iglesia del Nazareno en Barranquilla, Colombia. ¡Ven y visítanos!" />
       <script>{script}</script>
     </>
   );
